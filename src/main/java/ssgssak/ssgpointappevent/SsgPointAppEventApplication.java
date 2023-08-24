7d4f74ca70dde77e271bb5e4dcf9d0cfa4a7e2d6
kklee0930 <kklee0930@gmail.com>
2023-08-24 16:16:45 +0900

    Init: 이벤트 프로젝트 생성
    이벤트 프로젝트 생성했습니다.

---
+++ src/main/java/ssgssak/ssgpointappevent/SsgPointAppEventApplication.java
@@ -0,0 +1,13 @@
+package ssgssak.ssgpointappevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgPointAppEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgPointAppEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssgssak/ssgpointappevent/SsgPointAppEventApplicationTests.java
@@ -0,0 +1,13 @@
+package ssgssak.ssgpointappevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgPointAppEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
