056fb81807477a60802c11740263cc25fd782efc
Hwan0518 <qip0518@naver.com>
2023-09-18 00:39:56 +0900

    fix: CORS 설정 어노테이션 변경

--- src/main/java/ssgssak/ssgpointappevent/global/config/security/CorsConfig.java
+++ src/main/java/ssgssak/ssgpointappevent/global/config/security/CorsConfig.java
@@ -1,12 +1,12 @@
 package ssgssak.ssgpointappevent.global.config.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
